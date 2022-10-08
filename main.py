5eacebbbf3a99fb6d02f2097d96c1f23f128cc3d
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-10-09 02:07:49 +0530

    added files,planner,assets etc.

---
+++ main.py
@@ -0,0 +1,10 @@
+# VOTING MACHINE
+
+## DUE DATE! 14-10-22, today 9/10/22
+import csv
+import pyautogui as pg 
+#from tkinter import*
+import tkinter as tk 
+import sys #will be used to flush buffer data 
+
+
