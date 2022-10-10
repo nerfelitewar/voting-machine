6a73786e729bcc490cfe45b2d4257c4f02ef3284
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-10-11 03:37:23 +0530

    50% done with work- Changes made are normal
    Bugs- str is not added on voted to
    
    and all go to go...

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 # VOTING MACHINE
 
-## DUE DATE! 14-10-22, today 9/10/22
+## DUE DATE! 14-10-22, today 11/10/22
 import csv
 from colorama import Fore
 import time
 import pyautogui as pg 
-import sys #will be used to flush buffer data 
+import sys 
 
 
-
-#MaxVoteParty,Max_Votes=0,
 filename=(r"C:\MY CODING WORKSPACE\School_Project\Data\votes.csv")
-
-#fields=["NAME","AGE","ID","REGION","VOTED TO"]
-
-user=pg.prompt("Who are you?",title='IDENTIFICATION')
-age=pg.prompt("Enter your age.",title='YOUR AGE',default=18)
-id_no=pg.prompt("Enter your Identification number",title="UID")
-region=pg.prompt("Where are you from?",title="Location")
-str(user)
-str(age)
-str(id_no)
-str(region)
-voted_to=''
-
-#rows=[user,age,id_no,region,voted_to]
-
-
-
-time.sleep(1)
-
+fields=["NAME","AGE","ID","REGION","VOTED TO"]
+with open(filename,'w') as csvfile:
+    csvwriter = csv.writer(csvfile) 
+    csvwriter.writerow(fields)
+    #works fine
 F_ask_main=pg.confirm('Start',title="STARTING...",buttons=["Yes","No"])
-while F_ask_main=='Yes':
+voted_to=""
 
+
+'''''NO CHANGE HERE'''
+
+while F_ask_main=='Yes':
+    user=pg.prompt("Who are you?",title='IDENTIFICATION')
+    age=pg.prompt("Enter your age.",title='YOUR AGE',default=18)
+    id_no=pg.prompt("Enter your Identification number",title="UID")
+    region=pg.prompt("Where are you from?",title="Location")
+    voted_to=''
+
+    pg.alert("Press OK to continue",title="CONTINUE") 
     Ask_main=int(input(Fore.RED+"""
     1. Start voting\n
-    2. Show result \n
+    2. Show result \n 
     3. Exit
     -----------------
 
@@ -60,7 +55,8 @@ while F_ask_main=='Yes':
             5.CPI-M
             6.Skip Vote [Null]
         """+Fore.RESET))
-        vote_ask=pg.confirm('Choose your party',title='CHOOSE')
+        #~DONT DELETE THIS 
+        vote_ask=pg.confirm('Confirm this?',title='Check vote')
 
         if vote_ask==1:
             BJP+=1
@@ -79,64 +75,23 @@ while F_ask_main=='Yes':
             voted_to=voted_to.join("CPI-M")
         if vote_ask==6:
             SKIP_VOTERS+=1
-            voted_to=voted_to.join("NO VOTE [SKIPPED]")
-        
-        
+            voted_to=voted_to.join('SKIPPED')
         
         
 
 
 
-        fields=["NAME","AGE","ID","REGION","VOTED TO"]
-        rows=[user,age,id_no,region,voted_to]
-        with open(filename,'w') as csvfile:
+        
+        rows=[user,age,id_no,region,voted_to] 
+        with open(filename,'a') as csvfile:
             csvwriter = csv.writer(csvfile) 
-            csvwriter.writerow(fields)
-            csvwriter.writerows(rows)
+            csvwriter.writerow(rows)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    C_ask_main=pg.confirm('Want to do again?',title='Again',buttons=['Yes','No'])
-    if C_ask_main=='No':
-         print(Fore.BLUE+"Good Bye! Have a nice day👋"+Fore.RESET)
-         exit()
+    F_ask_main=pg.confirm('Want to do again?',title='Again',buttons=['Yes','No'])
+    if F_ask_main=='No':
+        print(Fore.BLUE+"Good Bye! Have a nice day👋"+Fore.RESET)
+        exit()
+    
     
 
