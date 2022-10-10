d431200d85456e122a3c2d13a8ba675ad60228af
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-10-11 05:00:13 +0530

    done

--- Main_Code/read_data.py
+++ Main_Code/read_data.py
@@ -6,4 +6,16 @@ from colorama import Fore
 
 filename=(r"C:\MY CODING WORKSPACE\School_Project\Data\votes.csv")
 with open(filename) as csvfile:
+    csvwriter = csv.reader(csvfile)
+    for data in csvfile:
+        csvfile.seek(0) 
+        for x in csvfile.readlines():
+            print(x)
+        #print(x)
+       
     
+
+  
+  
+  
+  
