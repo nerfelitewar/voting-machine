8f729b1f4fb97248ec9963fe6e74f896cb106eaf
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-10-11 04:35:12 +0530

    done

---
+++ Main_Code/read_data.py
@@ -0,0 +1,9 @@
+import csv
+import pyautogui as pg
+from time import sleep
+from colorama import Fore 
+
+
+filename=(r"C:\MY CODING WORKSPACE\School_Project\Data\votes.csv")
+with open(filename) as csvfile:
+    
