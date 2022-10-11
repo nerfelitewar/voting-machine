02a867893d0dc995a7604db9ea4af11724681537
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-10-11 19:02:48 +0530

    fixed read_data.py

--- read_data.py
+++ read_data.py
@@ -1,18 +1,15 @@
 import csv
-import pyautogui as pg
 from time import sleep
 from colorama import Fore 
 
-filename=(r"C:\MY CODING WORKSPACE\School_Project\Data\votes.csv")
-with open(filename) as csvfile:
-    csvwriter = csv.reader(csvfile)
-    for data in csvfile:
-        csvfile.seek(0) 
-        for x in csvfile.readlines():
-            print(x)
-        #print(x)
 
-
+def read_data():
+    filename=(r"C:\MY CODING WORKSPACE\School_Project\Data\votes.csv")
+    with open(filename) as csvfile:
+            csvfile.seek(0)
+            for x in csvfile.readlines():
+                print(x)
+read_data()
        
     
 
